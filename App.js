4bbf1a233aca22d386fec6474ea1e29f0cf76a53
uit2712 <phamquangvi456@gmail.com>
2020-04-03 01:22:10 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+} from 'react-native';
+
+function App() {
+    return (
+        <Text>Hello world!</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default App;
